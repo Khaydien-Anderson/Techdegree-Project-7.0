a6bf712e4d84fc109b3cfde76c64f77539cf14ae
K Andy <kdnanderson94@gmail.com>
2021-08-14 14:59:27 +0100

    Toggle switch + JS started

--- app.js
+++ app.js
@@ -12,4 +12,129 @@ alertBanner.addEventListener('click', e => {
         alertBanner.style.display = 'none';
     }
 
+});
+
+//LINE CHART BELOW
+
+let trafficCanvas = document.getElementById('traffic-chart');
+
+let trafficData = {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+        "4-10", "11-17", "18-24", "25-31"],
+        datasets: [{
+            data:[750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+                2500],
+            backgroundColor: 'rgba(116,119,191, .3)',
+            borderWidth: 1,
+        }]
+};
+
+let trafficOptions = {
+        // options: ["hourly", "daily", "weekly", "monthly"],
+        aspectRation: 2.5,
+        animation: {
+            duration: 0
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+// BAR CHART BELOW // 
+
+let dailyCanvas = document.getElementById('daily-chart');
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+        display: false
+        }
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+// DONUT CHART BELOW //
+
+let mobileCanvas = document.getElementById('donut-chart')
+
+const mobileData = {
+        labels: ["Desktop", "Tablet", "Phones"],
+        datasets: [{
+            label: '# of users',
+            data: [2000, 550, 500],
+            borderWidth: 0,
+            backgroundColor: [
+                '#7477BF',
+                '#78CF82',
+                '#51B6C8'
+           ]
+        }]
+};
+
+const mobileOptions = {
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+
+send.addEventListener('click', () => {
+    if (user.value === "" && message.value === "") {
+        alert('Both fields must be filled out.')
+    } else if (user.value === '') {
+        alert('Message field must be filled out.')
+    } else if (message.value === '') {
+        alert('User field must be filled out.')
+    } else {
+        alert('Your message was sent.')
+    }
+
 })
